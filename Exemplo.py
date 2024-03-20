e8d6cf899debcf30795859688534635e6082e0a2
teteufrota <161596189+teteufrota@users.noreply.github.com>
2024-03-20 09:47:03 -0300

    Add files via upload

---
+++ Exemplo.py
@@ -0,0 +1,32 @@
+print ("Matheus")
+
+numero = 10
+nome = "João"
+boleano = True
+
+a = 20
+b = 30
+c = 40
+d = 50
+frase = "Meu Valor é de quanto?"
+palavra = "Valor"
+
+media = (a + b + c + d) / 4
+print ("A media é", media)
+quadrado = (a * a)
+print ("O quadrado de um dos numeros é", quadrado)
+dobro = (c + c)
+print ("O dobro de um dos numeros é", dobro)
+letras = 5
+print ("A quantidade de letras da palavra é", letras)
+espaço = 4
+print ("A quantidade de espaços da frase é", espaço)
+
+maior = True
+menor = False
+
+resultado1 = a and b
+resultado2 = a or b
+resultado3 = not a
+
+
